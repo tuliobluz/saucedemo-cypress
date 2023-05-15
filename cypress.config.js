3c5cf62ff51b59b300b5424bc9d894ceddd84c6e
Tulio Bandeira Luz <tulio.bandeiraluz@kloeckner.com>
2023-05-15 10:08:40 +0200

    added slack notification

--- cypress.config.js
+++ cypress.config.js
@@ -5,11 +5,9 @@ module.exports = defineConfig({
   e2e: {
     // viewportWidth: process.env.WIDTH || 1920
     // viewportWidth: process.env.HEIGHT || 1080
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
       return require('./cypress/plugins/index.js')(on, config)
     },
-    baseUrl: process.env.ENV_ID || 'https://www.saucedemo.com/', 
+    baseUrl: process.env.ENV_ID || 'https://www.saucedemo.com/',
   },
 })
